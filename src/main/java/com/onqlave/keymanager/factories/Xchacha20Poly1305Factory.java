2db4f16117a4bb5b51ded0ccf7a7f885ab6f3722
C <cesc@onqlave.com>
2023-06-30 14:52:40 +0700

    feat: xchacha factory

--- src/main/java/com/onqlave/keymanager/factories/Xchacha20Poly1305Factory.java
+++ src/main/java/com/onqlave/keymanager/factories/Xchacha20Poly1305Factory.java
@@ -1,11 +1,12 @@
 package com.onqlave.keymanager.factories;
 
+import com.onqlave.keymanager.keys.Xchacha20Poly1350Key;
+import com.onqlave.keymanager.keys.Xchacha20Poly1350KeyData;
+import com.onqlave.keymanager.operations.Xchacha20Poly1305Operation;
+import com.onqlave.keymanager.primitives.Xchacha20Poly1305Aead;
 import com.onqlave.service.CPRNGService;
 import com.onqlave.service.IDService;
-import com.onqlave.types.AEAD;
-import com.onqlave.types.Key;
-import com.onqlave.types.KeyFactory;
-import com.onqlave.types.KeyOperation;
+import com.onqlave.types.*;
 
 public class Xchacha20Poly1305Factory implements KeyFactory {
     public IDService idService;
@@ -18,16 +19,66 @@ public class Xchacha20Poly1305Factory implements KeyFactory {
 
     @Override
     public Key NewKey(KeyOperation operation) throws Exception {
-        return null;
+        KeyFormat format = operation.GetFormat();
+        if (this.validateKeyFormat(format)) {
+            throw new Exception("aesGcmKeyFactory: invalid key format.");
+        }
+
+        byte[] keyData = this.randomService.GetRandomBytes(format.Size());
+        return new Xchacha20Poly1350Key(this.idService.NewKeyID(), operation,
+                new Xchacha20Poly1350KeyData(keyData, KeyMaterialType.KeyMaterialSYMMETRIC, 0));
     }
 
     @Override
     public Key NewKeyFromData(KeyOperation operation, byte[] keyData) throws Exception {
-        return null;
+        KeyFormat format = operation.GetFormat();
+        if (this.validateKeyFormat(format)) {
+            throw new Exception("aesGcmKeyFactory: invalid key format.");
+        }
+        return new Xchacha20Poly1350Key(this.idService.NewKeyID(), operation,
+                new Xchacha20Poly1350KeyData(keyData, KeyMaterialType.KeyMaterialSYMMETRIC, 0));
     }
 
     @Override
     public AEAD Primitive(Key key) throws Exception {
-        return null;
+        if (this.validateKey(key)) {
+            return null;
+        }
+        return new Xchacha20Poly1305Aead(this.randomService, key);
+    }
+
+    private boolean validateKey(Key key) throws Exception {
+        if (this.validateKeyVersion(key.Data().GetVersion(), Xchacha20Poly1305Operation.XCHACHA20_POLY1305_VERSION)) {
+            return false;
+        }
+
+        byte[] value = key.Data().GetValue();
+
+        if (this.validateXChaChaKeySize(value.length)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validateKeyFormat(KeyFormat format) {
+        if (this.validateXChaChaKeySize(format.Size())) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateKeyVersion(int version, int maxExpected) {
+        if (version > maxExpected) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateXChaChaKeySize(int sizeInBytes) {
+        if (sizeInBytes != 32) {
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/com/onqlave/keymanager/keys/Xchacha20Poly1350KeyData.java
+++ src/main/java/com/onqlave/keymanager/keys/Xchacha20Poly1350KeyData.java
@@ -1,15 +1,22 @@
 package com.onqlave.keymanager.keys;
 
 import com.onqlave.types.KeyData;
+import com.onqlave.types.KeyMaterialType;
 
 public class Xchacha20Poly1350KeyData implements KeyData {
     public String typeURL;
     public byte[] value;
-    public int keyMaterialType;
+    public KeyMaterialType keyMaterialType;
     public int version;
 
-    public Xchacha20Poly1350KeyData(String typeURL, byte[] value, int keyMaterialType, int version) {
+    public Xchacha20Poly1350KeyData(String typeURL, byte[] value, KeyMaterialType keyMaterialType, int version) {
         this.typeURL = typeURL;
+        this.value = value;
+        this.keyMaterialType = keyMaterialType;
+        this.version = version;
+    }
+
+    public Xchacha20Poly1350KeyData(byte[] value, KeyMaterialType keyMaterialType, int version) {
         this.value = value;
         this.keyMaterialType = keyMaterialType;
         this.version = version;

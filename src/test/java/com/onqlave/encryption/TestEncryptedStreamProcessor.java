6248310526d45c896486866e3158c225f08b0bfe
C <cesc@onqlave.com>
2023-07-05 17:43:46 +0700

    test: add encrypted stream processor writeHeader test

--- src/test/java/com/onqlave/encryption/TestEncryptedStreamProcessor.java
+++ src/test/java/com/onqlave/encryption/TestEncryptedStreamProcessor.java
@@ -1,7 +1,10 @@
 package com.onqlave.encryption;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
 import org.mockito.Mock;
@@ -9,40 +12,45 @@ import org.mockito.MockitoAnnotations;
 import static org.mockito.Mockito.*;
 
 import com.onqlave.types.Algorithm;
+import com.onqlave.types.AlgorithmDeserialiser;
 
 public class TestEncryptedStreamProcessor {
   @Mock
   private Algorithm algorithm;
 
+  private byte[] cipherData;
+  private byte[] inputData = {1, 2, 3, 4, 5, 6};
+
   public TestEncryptedStreamProcessor() {
     MockitoAnnotations.openMocks(this);
   }
 
-  @Test
-  public void TestWriteHeader() throws Exception{
-    byte[] inputData = {1, 2, 3};
-    when(algorithm.Serialise()).thenReturn(inputData);
+  @Before
+  public void prepareTest() throws Exception {
+    Algorithm algo = new Algorithm(0, "aes-gcm-128", inputData);
 
     ByteArrayOutputStream cipherStream = new ByteArrayOutputStream();
     PlainStreamProcessor processor = new PlainStreamProcessorImpl(cipherStream);
-    processor.WriteHeader(algorithm);
+    processor.WriteHeader(algo);
+    processor.WritePacket(inputData);
 
-    byte[] writtenArray = cipherStream.toByteArray();
-
-    assertArrayEquals(inputData, writtenArray);
-
-    verify(algorithm, times(1)).Serialise();
+    cipherData = cipherStream.toByteArray();
   }
 
   @Test
-  public void TestWritePacket() throws Exception {
-    byte[] inputData = {1, 2, 3, 4, 5, 6};
-    ByteArrayOutputStream cipherStream = new ByteArrayOutputStream();
-    PlainStreamProcessor processor = new PlainStreamProcessorImpl(cipherStream);
-    processor.WritePacket(inputData);
+  public void TestWriteHeader() throws Exception {
+    ByteArrayInputStream cipherStream = new ByteArrayInputStream(cipherData);
+    EncryptedStreamProcessor processor = new EncryptedStreamProcessorImpl(cipherStream);
+    AlgorithmDeserialiser algo = processor.ReadHeader();
 
-    byte[] writtenArray = cipherStream.toByteArray();
+    assertNotNull(algo);
+    assertEquals(algo.Version(), 0);
+    assertArrayEquals(algo.Key(), inputData);
+    assertEquals(algo.GetAlgorithm(), "aes-gcm-128");
+  }
 
-    assertEquals(inputData.length + 4, writtenArray.length);
+  @Test
+  public void TestWritePacket() {
+
   }
 }

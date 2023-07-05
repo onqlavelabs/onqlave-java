82b8f2c0c97835d844c3c14bb80809545bc355f6
C <cesc@onqlave.com>
2023-07-05 11:50:07 +0700

    test: add more test cases for aes-gcm-aead primitive

--- src/test/java/com/onqlave/keymanager/primitives/TestAESGCMAEAD.java
+++ src/test/java/com/onqlave/keymanager/primitives/TestAESGCMAEAD.java
@@ -24,7 +24,7 @@ public class TestAESGCMAEAD {
   }
 
   @Test
-  public void TestEncryptDecrypt() throws Exception {
+  public void TestEncryptDecrypt_16ByteKeySize() throws Exception {
     CPRNGService randomService = new CPRNGServiceImpl();
     byte[] keyVal = {1, 2, 3 , 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 , 14, 15, 16};
 
@@ -39,5 +39,70 @@ public class TestAESGCMAEAD {
     byte[] decryptedText = encryptor.Decrypt(cipherText, null);
 
     assertArrayEquals(plainText.getBytes(), decryptedText);
+  }
+
+  @Test
+  public void TestEncryptDecrypt_32ByteKeySize() throws Exception {
+    CPRNGService randomService = new CPRNGServiceImpl();
+    byte[] keyVal = {1, 2, 3 , 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 , 14, 15, 16, 1, 2, 3 , 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 , 14, 15, 16};
+
+    when(key.Data()).thenReturn(keydata);
+    when(keydata.GetValue()).thenReturn(keyVal);
+
+    AesGcmAead encryptor = new AesGcmAead(key, randomService);
+
+    String plainText = "this is a test plaintext";
+    byte[] cipherText = encryptor.Encrypt(plainText.getBytes(), null);
+
+    byte[] decryptedText = encryptor.Decrypt(cipherText, null);
+
+    assertArrayEquals(plainText.getBytes(), decryptedText);
+  }
+
+  @Test
+  public void TestEncryptDecrypt_WithAssociatedData() throws Exception {
+    CPRNGService randomService = new CPRNGServiceImpl();
+    byte[] keyVal = {1, 2, 3 , 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 , 14, 15, 16};
+
+    when(key.Data()).thenReturn(keydata);
+    when(keydata.GetValue()).thenReturn(keyVal);
+
+    AesGcmAead encryptor = new AesGcmAead(key, randomService);
+
+    String plainText = "this is a test plaintext";
+    String associatedData = "this is associated data";
+    byte[] cipherText = encryptor.Encrypt(plainText.getBytes(), associatedData.getBytes());
+
+    byte[] decryptedText = encryptor.Decrypt(cipherText, associatedData.getBytes());
+
+    assertArrayEquals(plainText.getBytes(), decryptedText);
+  }
+
+  @Test (expected = IllegalArgumentException.class)
+  public void TestEncryptDecrypt_NullPlainText() throws Exception {
+    CPRNGService randomService = new CPRNGServiceImpl();
+    byte[] keyVal = {1, 2, 3 , 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 , 14, 15, 16};
+
+    when(key.Data()).thenReturn(keydata);
+    when(keydata.GetValue()).thenReturn(keyVal);
+
+    AesGcmAead encryptor = new AesGcmAead(key, randomService);
+
+    byte[] plainText = null;
+
+    // throw new IllegalArgumentException
+    byte[] cipherText = encryptor.Encrypt(plainText, null);
+  }
+
+  @Test (expected = Exception.class)
+  public void TestEncryptDecrypt_InvalidKeySize() throws Exception {
+    CPRNGService randomService = new CPRNGServiceImpl();
+    byte[] keyVal = {1, 2, 3};
+
+    when(key.Data()).thenReturn(keydata);
+    when(keydata.GetValue()).thenReturn(keyVal);
+
+    // throw new Exception("invalid AES key size")
+    AesGcmAead encryptor = new AesGcmAead(key, randomService);
   }
 }

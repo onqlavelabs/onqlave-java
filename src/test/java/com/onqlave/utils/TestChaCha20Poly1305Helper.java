7aabd808b61a86b80ea9ddade795342fe338490d
C <cesc@onqlave.com>
2023-07-06 13:37:03 +0700

    feat: encrypt and decrypt using xchacha20poly1305

--- src/test/java/com/onqlave/utils/TestChaCha20Poly1305Helper.java
+++ src/test/java/com/onqlave/utils/TestChaCha20Poly1305Helper.java
@@ -3,26 +3,23 @@ package com.onqlave.utils;
 import com.onqlave.service.CPRNGServiceImpl;
 import org.junit.Test;
 
-import java.nio.charset.StandardCharsets;
-import java.security.NoSuchAlgorithmException;
-
 public class TestChaCha20Poly1305Helper {
 
     @Test
-    public void TestEncDecChaChaPoly() throws Exception {
+    public void TestXChaCha20Poly1305() throws Exception {
         String plainText = "this is a plain text";
         CPRNGServiceImpl random = new CPRNGServiceImpl();
         byte[] key = random.GetRandomBytes(32);
         byte[] none = random.GetRandomBytes(12);
 
 
-        byte[] encData = ChaCha20Poly1305Helper.Encrypt(plainText.getBytes(), key, none);
+        byte[] encData = XChaCha20Poly1305Helper.Encrypt(key, plainText.getBytes(), new byte[0]);
 
 
         System.out.println(encData.toString());
 
         System.out.println(new String(none));
-        byte[] pt = ChaCha20Poly1305Helper.Decrypt(encData, key);
+        byte[] pt = XChaCha20Poly1305Helper.Decrypt(key, encData, new byte[0]);
 
 
         System.out.println(new String(pt));

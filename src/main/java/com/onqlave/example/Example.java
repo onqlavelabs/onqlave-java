d96dcffe097b8d2d02737e96f4c7744e7d210da5
C <cesc@onqlave.com>
2023-07-10 22:51:32 +0700

    improve: remove unused variable

--- src/main/java/com/onqlave/example/Example.java
+++ src/main/java/com/onqlave/example/Example.java
@@ -18,25 +18,10 @@ public class Example {
     // Reset
     public static final String RESET = "\u001B[0m";
 
-    // Regular Colors
-    public static final String BLACK = "\u001B[30m";
-    public static final String RED = "\u001B[31m";
-    public static final String GREEN = "\u001B[32m";
-    public static final String YELLOW = "\u001B[33m";
-    public static final String BLUE = "\u001B[34m";
-    public static final String PURPLE = "\u001B[35m";
-    public static final String CYAN = "\u001B[36m";
-    public static final String WHITE = "\u001B[37m";
-
     // Bold Colors
-    public static final String BLACK_BOLD = "\u001B[30;1m";
     public static final String RED_BOLD = "\u001B[31;1m";
     public static final String GREEN_BOLD = "\u001B[32;1m";
     public static final String YELLOW_BOLD = "\u001B[33;1m";
-    public static final String BLUE_BOLD = "\u001B[34;1m";
-    public static final String PURPLE_BOLD = "\u001B[35;1m";
-    public static final String CYAN_BOLD = "\u001B[36;1m";
-    public static final String WHITE_BOLD = "\u001B[37;1m";
 
     public static void main(String args[]) {
 
